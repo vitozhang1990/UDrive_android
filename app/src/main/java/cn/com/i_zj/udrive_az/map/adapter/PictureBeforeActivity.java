67b3a9e7820671a8830e63cea2eb334b2563d2de
vitozhang <vitozhang1990@icloud.com>
2019-01-08 21:36:21 +0800

    车损拍照-适配问题

--- app/src/main/java/cn/com/i_zj/udrive_az/map/adapter/PictureBeforeActivity.java
+++ app/src/main/java/cn/com/i_zj/udrive_az/map/adapter/PictureBeforeActivity.java
@@ -3,13 +3,18 @@ package cn.com.i_zj.udrive_az.map.adapter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.CheckBox;
 
+import butterknife.BindView;
 import butterknife.OnClick;
 import cn.com.i_zj.udrive_az.DBSBaseActivity;
 import cn.com.i_zj.udrive_az.R;
 import cn.com.i_zj.udrive_az.map.MapUtils;
 
 public class PictureBeforeActivity extends DBSBaseActivity {
+
+    @BindView(R.id.left_before)
+    CheckBox left_before;
 
     @Override
     protected int getLayoutResource() {
@@ -20,6 +25,7 @@ public class PictureBeforeActivity extends DBSBaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MapUtils.statusBarColor(this);
+        left_before.setChecked(true);
     }
 
     @OnClick({R.id.iv_back})
