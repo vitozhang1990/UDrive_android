55899a04794e4983baab92eb3bbb8413007c2248
vitozhang <vitozhang1990@icloud.com>
2019-01-10 20:01:21 +0800

    update

--- app/src/main/java/cn/com/i_zj/udrive_az/widget/MaskPierceView.java
+++ app/src/main/java/cn/com/i_zj/udrive_az/widget/MaskPierceView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
@@ -16,7 +17,7 @@ import android.view.ViewGroup;
 public class MaskPierceView extends View {
     private int mScreenWidth;   // 屏幕的宽
     private int mScreenHeight;  // 屏幕的高
-    private Paint mDstPaint, mSrcPaint;
+    private Paint mDstPaint, mSrcPaint, mStrokePaint, mSolidlinePaint;
 
     private int viewWidth;
     private int viewHeight;
@@ -25,8 +26,9 @@ public class MaskPierceView extends View {
     private float endX = 0;
     private float endY = 0;
 
-    private RectF rectF;
+    private RectF rectF, rectFStroke;
     private int color = Color.BLACK;
+    private boolean showStroke;
 
     public MaskPierceView(Context context) {
         this(context, null);
@@ -47,10 +49,25 @@ public class MaskPierceView extends View {
 
         mDstPaint = new Paint();
         mSrcPaint = new Paint();
+        mStrokePaint = new Paint();
+        mSolidlinePaint = new Paint();
+
         mDstPaint.setColor(Color.YELLOW);
+
         mSrcPaint.setColor(Color.BLUE);
         mSrcPaint.setAlpha(160);
         mSrcPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT));
+
+        mStrokePaint.setStyle(Paint.Style.STROKE);
+        mStrokePaint.setAntiAlias(true);
+        mStrokePaint.setStrokeWidth(6);
+        mStrokePaint.setColor(Color.parseColor("#7A7A7A"));
+        mStrokePaint.setPathEffect(new DashPathEffect(new float[]{10, 10}, 0));
+
+        mSolidlinePaint.setStyle(Paint.Style.STROKE);
+        mSolidlinePaint.setAntiAlias(true);
+        mSolidlinePaint.setStrokeWidth(6);
+        mSolidlinePaint.setColor(Color.parseColor("#7A7A7A"));
         setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     }
 
@@ -63,6 +80,7 @@ public class MaskPierceView extends View {
         endX = viewWidth - 150;
         endY = viewHeight - 300;
         rectF = new RectF(startX, startY, endX, endY);
+        rectFStroke = new RectF(startX, startY, endX, endY);
     }
 
     @Override
@@ -76,14 +94,22 @@ public class MaskPierceView extends View {
         canvas1.drawRect(new RectF(0, 0, mScreenWidth, mScreenHeight), paint);
 
         canvas.drawBitmap(bm, 0, 0, mSrcPaint);
+
+        if (showStroke) {
+            canvas.drawRoundRect(rectFStroke, 50, 50, mStrokePaint);
+        } else {
+            canvas.drawRoundRect(rectFStroke, 50, 50, mSolidlinePaint);
+        }
     }
 
     public void black(boolean black) {
         if (black) {
             mSrcPaint.setAlpha(255);
+            showStroke = true;
             color = Color.parseColor("#1C1C1C");
         } else {
             mSrcPaint.setAlpha(160);
+            showStroke = false;
             color = Color.BLACK;
         }
         invalidate();
